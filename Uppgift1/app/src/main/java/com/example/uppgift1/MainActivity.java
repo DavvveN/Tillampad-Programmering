f863b86f49e80a74a1f328020d00f03189c2cd5e
David Nilsson <65786975+DavvveN@users.noreply.github.com>
2022-10-12 10:09:57 +0200

    misc

--- Uppgift1/app/src/main/java/com/example/uppgift1/MainActivity.java
+++ Uppgift1/app/src/main/java/com/example/uppgift1/MainActivity.java
@@ -6,17 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private TextView zero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView)findViewById(R.id.show_count);
+        zero = (TextView)findViewById(R.id.button_Zero);
     }
 
     public void showToast(View view) {
@@ -28,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
+            zero.setBackgroundColor(173);
         }
+    }
+    public void reset(View view){
+        mCount = 0;
+        mShowCount.setText(0);
+        mShowCount.setBackgroundColor(808080);
     }
 }
